436027963978b6b20bbbe7673f227d9839abbc7f
Rahul <rsiharu@gmail.com>
2021-05-13 22:22:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express=require('express');
+const cookieParser=require('cookie-Parser');
+const app=express();
+const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+//used for session cookie
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+const { pass } = require('./config/mongoose');
+const sassMiddleware=require('node-sass-middleware');
+//const MongoStore = require('connect-mongo').session();//MongoDB session store for Connect and Express 
+
+app.use(sassMiddleware({
+    src:'./assests/scss',
+    dest:'./assests/css', //destination
+    debug:true,  //show erros
+    outputStyle:'extended',
+    prefix:'/css'
+}));
+
+app.use(express.urlencoded({extended: true})); 
+app.use(cookieParser());
+app.use(express.static('./assests'));
+app.use(expressLayouts);
+
+//extract style and scripts from sub pages into layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100) //time after which cookie expires
+     }
+    // store: MongoStore.create(
+    //     {
+    //         mongooseConnection: db,
+    //         autoRemove: 'disabled'
+        
+    //     },
+    //     function(err){
+    //         console.log(err ||  'connect-mongodb setup ok');
+    //     }
+    //)
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+//use express router
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`server is running port: ${port}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const router=express.Router();
+const passport=require('passport');
+
+const usercontroller=require('../controllers/user_controller'); 
+
+router.get('/profile',passport.checkAuthentication,usercontroller.profile);
+
+// router.get('/profile',usercontroller.profile);
+
+router.get('/sign-up',usercontroller.signUp);
+router.get('/sign-in',usercontroller.signIn);
+
+
+router.post('/create',usercontroller.create);
+//router.post('/create-session',usercontroller.createSession);
+
+//use passport as middleware to autenticate
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'},
+),usercontroller.createSession);
+
+
+router.get('/sign-out',usercontroller.destroySession);
+module.exports=router;
