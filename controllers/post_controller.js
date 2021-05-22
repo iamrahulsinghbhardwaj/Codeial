bc54f660c0f4583c3b23dc02055909c2402498e8
Rahul <rsiharu@gmail.com>
2021-05-22 14:43:13 +0530

    Adding more flash message

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -7,21 +7,21 @@ module.exports.create= async function(req,res){
             content:req.body.content,
             user:req.user._id
         });
-
+        
+        req.flash('success','Post Published');
         return res.redirect('back');
 
     }catch(err){
-        console.log('Error',err);
+        req.flash('error',err);
         return;
-    }
-    
-        
+    }        
     
 }
 
 module.exports.destroy=async function(req,res){ 
 
     try{
+         
         let post=await  Post.findById(req.params.id);
 
         // .id means converting the object id to string
@@ -29,13 +29,17 @@ module.exports.destroy=async function(req,res){
             post.remove();
 
            await Comment.deleteMany({post:req.params.id});
+           req.flash('success','Post and associated comments deleted!');
            return res.redirect('back');
 
         }else{
+            req.flash('err','You cannot delete this post');
+
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error',err);
+       
+        req.flash('error',err);
         return;
     }
        
