236c8809387c186646767b970576e133f0ea6898
Rahul <rsiharu@gmail.com>
2021-05-24 17:40:51 +0530

    Deleting a Post

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -4,6 +4,7 @@ const User=require('../models/user');
 module.exports.home=async function(req,res){
       try{
             let posts=await Post.find({})
+      .sort('-createdAt') //sorting by near
      .populate('user')
      .populate({
            path:'comments',
