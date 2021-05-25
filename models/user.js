5efd6c6965d75cc7c2ae312ab634fb32ad26d4a4
Rahul <rsiharu@gmail.com>
2021-05-25 15:56:20 +0530

    Saving Files from Params

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 
 const multer=require('multer');
-const Path=require('path');
+const path=require('path');
 
-const AVATAR_PATH=path.join('/uploads/avatars');
+const AVATAR_PATH=path.join('/uploads/users/avatars');
 
 
 const userSchema=new mongoose.Schema({
@@ -32,9 +32,13 @@ let storage = multer.diskStorage({
       cb(null, path.join(__dirname,'..',AVATAR_PATH));
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now());
     }
 });
+
+//static methords
+userSchema.statics.uploadedAvatar=multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath=AVATAR_PATH;
 
 
 const User=mongoose.model('User',userSchema);
